82ca92f5e07a6ce64e0f9fb3dce8370cb5e7f568
cjy <cjylzsh@cjydeMacBook-Pro.local>
2026-06-05 09:51:33 +0800

    first commit

---
+++ date_utils.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+ 
+def days_between(date1, date2):
+    d1 = datetime.strptime(date1, "%Y-%m-%d")
+    d2 = datetime.strptime(date2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
