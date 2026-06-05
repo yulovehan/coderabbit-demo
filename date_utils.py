f95edbdf7ae38c4937fd4299f3718ee49b6c38e8
cjy <cjylzsh@cjydeMacBook-Pro.local>
2026-06-05 09:58:53 +0800

    fix: broken version bug

--- date_utils.py
+++ date_utils.py
@@ -1,6 +1,9 @@
 from datetime import datetime
  
 def days_between(date1, date2):
-    d1 = datetime.strptime(date1, "%Y-%m-%d")
+    # 故意改成错误的格式（一个用斜杠，一个用横杆）
+    d1 = datetime.strptime(date1, "%Y/%m/%d")
     d2 = datetime.strptime(date2, "%Y-%m-%d")
-    return abs((d2 - d1).days)
+ 
+    # 故意去掉 abs，导致负数结果
+    return (d2 - d1).days
